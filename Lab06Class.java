a73d6ed7338bb2fa2db6e80a33116b8ddca31b5a
GatorSethBarber <barber.seth@ufl.edu>
2022-03-03 15:15:58 -0500

    First commit for COP3502C

---
+++ Lab06Class.java
@@ -0,0 +1,11 @@
+public class Lab06Class {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 2;
+        int result;
+
+        result = number1 + number2;
+
+        System.out.println("The result of operating on " + number1 + " and " + number2 + " is " + result + ".");
+    }
+}
