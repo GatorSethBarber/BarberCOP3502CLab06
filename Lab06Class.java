8f6ff309289de745c84552c7fe60a6cece10ceef
GatorSethBarber <barber.seth@ufl.edu>
2022-03-03 15:39:48 -0500

    Second commit for the lab

--- Lab06Class.java
+++ Lab06Class.java
@@ -1,11 +1,17 @@
+/**
+ * Updated version of the code for Lab 6 for COP3502C-10852.
+ * Available at https://github.com/GatorSethBarber/BarberCOP3502CLab06
+ * @author Seth Barber
+ *
+ */
+
 public class Lab06Class {
     public static void main(String[] args) {
         int number1 = 1;
         int number2 = 2;
         int result;
 
-        result = number1 + number2;
-
+        result = number1 - number2;
         System.out.println("The result of operating on " + number1 + " and " + number2 + " is " + result + ".");
     }
 }
